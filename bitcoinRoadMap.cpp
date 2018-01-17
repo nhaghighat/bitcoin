0daff9d6fe720284668948226e1fce3874d49bb1
Unknown <navid.haghighat@gmail.com>
2018-01-17 19:29:09 +0330

    bitcoin roadmap

---
+++ bitcoinRoadMap.cpp
@@ -0,0 +1,6 @@
+bitcoind.cpp:
+int main(int argc, char* argv[])
+  return (AppInit(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE);
+    gArgs.ParseParameters(argc, argv);
+      mapArgs[str] = strValue;
+    
