b18ecdd8faec7f0bc9cd2e80ac8310e341a1673a
Unknown <navid.haghighat@gmail.com>
2018-01-19 12:15:54 +0330

    adde some parts

--- bitcoinRoadMap.cpp
+++ bitcoinRoadMap.cpp
@@ -3,4 +3,12 @@ int main(int argc, char* argv[])
   return (AppInit(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE);
     gArgs.ParseParameters(argc, argv);
       mapArgs[str] = strValue;
-    
+    gArgs.ReadConfigFile(gArgs.GetArg("-conf", BITCOIN_CONF_FILENAME));
+    SelectParams(ChainNameFromCommandLine());
+      ChainNameFromCommandLine()
+        bool fRegTest = gArgs.GetBoolArg("-regtest", false);
+        bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+      SelectBaseParams(network);
+        globalChainBaseParams = CreateBaseChainParams(chain);
+      globalChainParams = CreateChainParams(network);
+        return std::unique_ptr<CChainParams>(new CMainParams());
